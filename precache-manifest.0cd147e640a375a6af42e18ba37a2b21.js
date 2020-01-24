79dae1509036690399fa893a7913b818306a4aa8
Josh Newton <josh.newton@learnsci.co.uk>
2020-01-24 17:02:33 +0000

    Updates

---
+++ precache-manifest.0cd147e640a375a6af42e18ba37a2b21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38dc905d0a30cfabbe04868a9794ec65",
+    "url": "/neilmaggs.co.uk/index.html"
+  },
+  {
+    "revision": "abf437e5994baa5cefdb",
+    "url": "/neilmaggs.co.uk/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aa7416a0520fb31b0d29",
+    "url": "/neilmaggs.co.uk/static/js/2.76e7a864.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/neilmaggs.co.uk/static/js/2.76e7a864.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abf437e5994baa5cefdb",
+    "url": "/neilmaggs.co.uk/static/js/main.e7590fbf.chunk.js"
+  },
+  {
+    "revision": "cadf308e206fccb6cdfa",
+    "url": "/neilmaggs.co.uk/static/js/runtime-main.50a474ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neilmaggs.co.uk/static/media/logo.5d5d9eef.svg"
+  }
+]);
